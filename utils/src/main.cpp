a3257c28eccf0f16ac46c7a902c68ef60e36e46f
Jonathan Michael Thomas <echidnaman@kubuntu.org>
2010-06-01 17:44:40 +0000

    Initial import of batch installer. Basically works, but could have a few more nice features and definitely needs a bit of polish. Drop in replacement for install-package, though.
    svn path=/trunk/playground/libs/libqapt/; revision=1133414

---
+++ utils/src/main.cpp
@@ -0,0 +1,79 @@
+/***************************************************************************
+ *   Copyright © 2010 Jonathan Thomas <echidnaman@kubuntu.org>             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version        *
+ *   accepted by the membership of KDE e.V. (or its successor approved     *
+ *   by the membership of KDE e.V.), which shall act as a proxy            *
+ *   defined in Section 14 of version 3 of the license.                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#include "qaptbatch.h"
+#include <KApplication>
+#include <KAboutData>
+#include <KCmdLineArgs>
+#include <KLocale>
+
+static const char description[] =
+    I18N_NOOP("A batch installer using QApt");
+
+static const char version[] = "0.1";
+
+int main(int argc, char **argv)
+{
+    KAboutData about("qaptbatch", 0, ki18n("QApt Batch Installer"), version, ki18n(description),
+                     KAboutData::License_GPL, ki18n("(C) 2010 Jonathan Thomas"), KLocalizedString(), 0, "echidnaman@kubuntu.org");
+    about.addAuthor( ki18n("Jonathan Thomas"), KLocalizedString(), "echidnaman@kubuntu.org" );
+    KCmdLineArgs::init(argc, argv, &about);
+
+    KCmdLineOptions options;
+    options.add("attach <winid>", ki18nc("@info:shell","Attaches the window to an X app specified by winid"));
+    options.add("install", ki18nc("@info:shell", "Install a package"));
+    options.add("uninstall", ki18nc("@info:shell", "Remove a package"));
+    options.add("update", ki18nc("@info:shell", "Update the package cache"));
+    options.add("+[Package(s)]", ki18nc("@info:shell", "Packages to be operated upon"));
+    KCmdLineArgs::addCmdLineOptions(options);
+
+    int winId;
+    QString mode;
+
+    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+
+    if (args->isSet("attach") && args->count() > 0) {
+        winId = args->getOption("attach").toInt();
+    } else {
+        winId = 0;
+    }
+
+    if (args->isSet("install") && args->count() > 0) {
+        mode = QString("install");
+    } else if (args->isSet("uninstall") && args->count() > 0) {
+        mode = QString("uninstall");
+    } else if (args->isSet("update")) {
+        mode = QString("update");
+    } else {
+        return 1;
+    }
+
+    QStringList packages;
+
+    for(int i = 0; i < args->count(); i++) { // Counting start at 0!
+        packages << args->arg(i);
+    }
+
+    KApplication app;
+
+    QAptBatch *batchInstaller = new QAptBatch(mode, packages, winId);
+
+    return app.exec();
+}
---
+++ utils/src/qaptbatch.cpp
@@ -0,0 +1,204 @@
+/***************************************************************************
+ *   Copyright © 2010 Jonathan Thomas <echidnaman@kubuntu.org>             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version        *
+ *   accepted by the membership of KDE e.V. (or its successor approved     *
+ *   by the membership of KDE e.V.), which shall act as a proxy            *
+ *   defined in Section 14 of version 3 of the license.                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#include "qaptbatch.h"
+
+// Qt includes
+#include <QtDBus/QDBusConnection>
+#include <QtDBus/QDBusMessage>
+#include <QtDBus/QDBusPendingCall>
+#include <QtDBus/QDBusServiceWatcher>
+#include <QtGui/QLabel>
+#include <QtGui/QVBoxLayout>
+
+#include <KDebug>
+#include <KIcon>
+#include <KLocalizedString>
+#include <KWindowSystem>
+
+#include <libqapt/globals.h>
+#include <libqapt/package.h>
+
+QAptBatch::QAptBatch(QString mode, QStringList packages, int winId)
+    : KProgressDialog()
+    , m_watcher(0)
+    , m_mode(mode)
+    , m_packages(packages)
+    , m_vbox(0)
+    , m_progressLabel(0)
+{
+    QDBusConnection::systemBus().connect("org.kubuntu.qaptworker", "/", "org.kubuntu.qaptworker",
+                                "errorOccurred", this, SLOT(errorOccurred(int, const QVariantMap&)));
+    QDBusConnection::systemBus().connect("org.kubuntu.qaptworker", "/", "org.kubuntu.qaptworker",
+                                "workerStarted", this, SLOT(workerStarted()));
+    QDBusConnection::systemBus().connect("org.kubuntu.qaptworker", "/", "org.kubuntu.qaptworker",
+                                "workerEvent", this, SLOT(workerEvent(int)));
+    QDBusConnection::systemBus().connect("org.kubuntu.qaptworker", "/", "org.kubuntu.qaptworker",
+                                "workerFinished", this, SLOT(workerFinished(bool)));
+
+    m_watcher = new QDBusServiceWatcher(this);
+    m_watcher->setConnection(QDBusConnection::systemBus());
+    m_watcher->setWatchMode(QDBusServiceWatcher::WatchForOwnerChange);
+    m_watcher->addWatchedService("org.kubuntu.qaptworker");
+
+    kDebug() << m_mode;
+
+    if (m_mode == "install") {
+        commitChanges(QApt::Package::ToInstall);
+    } else if (m_mode == "uninstall") {
+        commitChanges(QApt::Package::ToRemove);
+    } else if (m_mode == "update") {
+        QDBusMessage message;
+        message = QDBusMessage::createMethodCall("org.kubuntu.qaptworker",
+                  "/",
+                  "org.kubuntu.qaptworker",
+                  QLatin1String("updateCache"));
+        QDBusConnection::systemBus().asyncCall(message);
+    }
+
+    if (winId != 0) {
+        KWindowSystem::setMainWindow(this, winId);
+    }
+
+    setWindowIcon(KIcon("applications-other"));
+    setAutoClose(false);
+}
+
+QAptBatch::~QAptBatch()
+{
+}
+
+void QAptBatch::commitChanges(int mode)
+{
+    QMap<QString, QVariant> instructionList;
+
+    foreach (const QString &package, m_packages) {
+        instructionList.insert(package, mode);
+    }
+
+    QDBusMessage message;
+    message = QDBusMessage::createMethodCall("org.kubuntu.qaptworker",
+              "/",
+              "org.kubuntu.qaptworker",
+              QLatin1String("commitChanges"));
+
+    QList<QVariant> args;
+    args << QVariant(instructionList);
+    message.setArguments(args);
+    QDBusConnection::systemBus().asyncCall(message);
+}
+
+void QAptBatch::cancelDownload()
+{
+    QDBusMessage message;
+    message = QDBusMessage::createMethodCall("org.kubuntu.qaptworker",
+              "/",
+              "org.kubuntu.qaptworker",
+              QLatin1String("cancelDownload"));
+    QDBusConnection::systemBus().asyncCall(message);
+}
+
+void QAptBatch::workerStarted()
+{
+    kDebug() << "worker started";
+    connect(m_watcher, SIGNAL(serviceOwnerChanged(QString,QString,QString)),
+            this, SLOT(serviceOwnerChanged(QString, QString, QString)));
+
+    QDBusConnection::systemBus().connect("org.kubuntu.qaptworker", "/", "org.kubuntu.qaptworker",
+                                "downloadProgress", this, SLOT(updateDownloadProgress(int, int, int)));
+    QDBusConnection::systemBus().connect("org.kubuntu.qaptworker", "/", "org.kubuntu.qaptworker",
+                                "downloadMessage", this, SLOT(updateDownloadMessage(int, const QString&)));
+    QDBusConnection::systemBus().connect("org.kubuntu.qaptworker", "/", "org.kubuntu.qaptworker",
+                                "commitProgress", this, SLOT(updateCommitProgress(const QString&, int)));
+}
+
+void QAptBatch::workerEvent(int code)
+{
+    switch (code) {
+        case QApt::Globals::CacheUpdateStarted:
+            connect(this, SIGNAL(cancelClicked()), this, SLOT(cancelDownload()));
+            setWindowTitle(i18n("Refreshing package information"));
+            setLabelText(i18n("Downloading package information"));
+            show();
+            break;
+        case QApt::Globals::CacheUpdateFinished:
+            setLabelText(i18n("Package information successfully refreshed"));
+            disconnect(this, SIGNAL(cancelClicked()), this, SLOT(cancelDownload()));
+            break;
+        case QApt::Globals::PackageDownloadStarted:
+            connect(this, SIGNAL(cancelClicked()), this, SLOT(cancelDownload()));
+            setWindowTitle(i18n("Downloading Packages"));
+            setLabelText(i18n("Downloading packages"));
+            show();
+            break;
+        case QApt::Globals::PackageDownloadFinished:
+            setAllowCancel(false);
+            disconnect(this, SIGNAL(cancelClicked()), this, SLOT(cancelDownload()));
+            break;
+        case QApt::Globals::CommitChangesStarted:
+            setWindowTitle(i18n("Installing Packages"));
+            show(); // In case no download was necessary
+            break;
+        case QApt::Globals::CommitChangesFinished:
+            setLabelText(i18np("Package successfully installed", "Packages successfully installed", m_packages.size()));
+            progressBar()->setValue(100);
+            break;
+    }
+}
+
+void QAptBatch::workerFinished(bool success)
+{
+    disconnect(m_watcher, SIGNAL(serviceOwnerChanged(QString,QString,QString)),
+               this, SLOT(serviceOwnerChanged(QString, QString, QString)));
+
+    QDBusConnection::systemBus().disconnect("org.kubuntu.qaptworker", "/", "org.kubuntu.qaptworker",
+                                "downloadProgress", this, SLOT(updateDownloadProgress(int, int, int)));
+    QDBusConnection::systemBus().disconnect("org.kubuntu.qaptworker", "/", "org.kubuntu.qaptworker",
+                                "downloadMessage", this, SLOT(updateDownloadMessage(int, const QString&)));
+    QDBusConnection::systemBus().disconnect("org.kubuntu.qaptworker", "/", "org.kubuntu.qaptworker",
+                                "commitProgress", this, SLOT(updateCommitProgress(const QString&, int)));
+}
+
+void QAptBatch::serviceOwnerChanged(const QString &name, const QString &oldOwner, const QString &newOwner)
+{
+    if (oldOwner.isEmpty()) {
+        return; // Don't care, just appearing
+    }
+
+    if (newOwner.isEmpty()) {
+        qDebug() << "It looks like our worker got lost";
+
+        //TODO: KMessageBox saying worker died
+    }
+}
+
+void QAptBatch::updateDownloadProgress(int percentage, int speed, int ETA)
+{
+    progressBar()->setValue(percentage);
+    //TODO: speed, ETA
+}
+
+void QAptBatch::updateCommitProgress(const QString& message, int percentage)
+{
+    progressBar()->setValue(percentage);
+    setLabelText(message);
+}
+
+#include "qaptbatch.moc"
