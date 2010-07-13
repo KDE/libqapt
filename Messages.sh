e823d2c7155b4d040fcc32991cf80b3e93d9601b
Jonathan Michael Thomas <echidnaman@kubuntu.org>
2010-07-13 23:41:37 +0000

    Move Messages.sh to the utils dir, so that our po/ dir will show up there in the tarballs, and our KDE-having CMakeLists.txt there will be able to handle the KDE translation macros
    svn path=/trunk/playground/libs/libqapt/; revision=1149663

--- Messages.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-$XGETTEXT `find utils/qapt-batch -name '*.cpp'` -o $podir/qaptbatch.pot
---
+++ utils/Messages.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+$XGETTEXT `find qapt-batch -name '*.cpp'` -o $podir/qaptbatch.pot
