d30e1bae3f799d610e3e60f8533a40871d3b6673
Jonathan Michael Thomas <echidnaman@kubuntu.org>
2010-11-24 20:50:49 +0000

    Add QApt::History, a class giving an interface to the APT history logfiles.
    svn path=/trunk/extragear/sysadmin/libqapt/; revision=1200371

---
+++ src/history.cpp
@@ -0,0 +1,224 @@
+/***************************************************************************
+ *   Copyright © 2010 Jonathan Thomas <echidnaman@kubuntu.org>             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version        *
+ *   accepted by the membership of KDE e.V. (or its successor approved     *
+ *   by the membership of KDE e.V.), which shall act as a proxy            *
+ *   defined in Section 14 of version 3 of the license.                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#include "history.h"
+
+#include <QtCore/QDir>
+#include <QtCore/QFile>
+#include <QtCore/QFileInfo>
+#include <QtCore/QProcess>
+#include <QDebug>
+
+namespace QApt {
+
+class HistoryItemPrivate
+{
+    public:
+        HistoryItemPrivate(const QString &data)
+        {
+            parseData(data);
+        };
+
+        // Data
+        QDateTime startDate;
+        Package::State action;
+        QStringList packageList;
+
+        void parseData(const QString &data);
+};
+
+void HistoryItemPrivate::parseData(const QString &data)
+{
+    QStringList lines = data.split(QLatin1Char('\n'));
+
+    int lineIndex = 0;
+    bool dateFound = false;
+    bool actionFound = false;
+    bool packageListFound = false;
+
+    QStringList actionStrings;
+    actionStrings << "Install" << "Upgrade" << "Downgrade" << "Remove" << "Purge";
+
+    while (lineIndex < lines.size()) {
+        QString line = lines.at(lineIndex);
+        // skip empty lines and lines beginning with '#'
+        if (line.isEmpty() || line.at(0) == '#') {
+            lineIndex++;
+            continue;
+        }
+
+        QStringList keyValue = line.split(QLatin1String(": "));
+        int actionIndex = actionStrings.indexOf(keyValue.value(0));
+
+        // Invalid
+        if (keyValue.size() < 2) {
+            lineIndex++;
+            continue;
+        }
+
+        if (!dateFound && (keyValue.value(0) == QLatin1String("Start-Date"))) {
+            startDate = QDateTime::fromString(keyValue.value(1), QLatin1String("yyyy-MM-dd  hh:mm:ss"));
+        } else if (!actionFound && (actionIndex > -1)) {
+            switch(actionIndex) {
+            case 0:
+                action = Package::ToInstall;
+                break;
+            case 1:
+                action = Package::ToUpgrade;
+                break;
+            case 2:
+                action = Package::ToDowngrade;
+                break;
+            case 3:
+                action = Package::ToRemove;
+                break;
+            case 4:
+                action = Package::ToPurge;
+                break;
+            default:
+                break;
+            }
+
+            QString packages = keyValue.value(1);
+            // Remove arch info
+            packages.remove(QRegExp(":\\w+"));
+
+            foreach (QString package, packages.split("),")) {
+                package.append(QLatin1Char(')'));
+                packageList << package;
+            }
+        }
+
+        lineIndex++;
+    }
+}
+
+HistoryItem::HistoryItem(const QString &data)
+       : d_ptr(new HistoryItemPrivate(data))
+{
+}
+
+HistoryItem::~HistoryItem()
+{
+    delete d_ptr;
+}
+
+QDateTime HistoryItem::startDate() const
+{
+    Q_D(const HistoryItem);
+
+    return d->startDate;
+}
+
+Package::State HistoryItem::action() const
+{
+    Q_D(const HistoryItem);
+
+    return d->action;
+}
+
+QStringList HistoryItem::packageList() const
+{
+    Q_D(const HistoryItem);
+
+    return d->packageList;
+}
+
+
+
+class HistoryPrivate
+{
+    public:
+        HistoryPrivate(const QString &fileName) : historyFilePath(fileName)
+        {
+            init();
+        };
+
+        // Data
+        QString historyFilePath;
+        HistoryItemList historyItemList;
+
+        void init();
+};
+
+void HistoryPrivate::init()
+{
+    qDeleteAll(historyItemList);
+    historyItemList.clear();
+
+    QString data;
+
+    QFileInfo historyFile(historyFilePath);
+    QString directoryPath = historyFile.absoluteDir().absolutePath();
+    QDir logDirectory(directoryPath);
+    QStringList logFiles = logDirectory.entryList(QDir::Files, QDir::Name);
+
+    foreach (QString file, logFiles) {
+        file.prepend(directoryPath + QLatin1Char('/'));
+        if (file.contains(QLatin1String("history"))) {
+            if (file.endsWith(QLatin1String(".gz"))) {
+                QProcess gunzip;
+                gunzip.start(QLatin1String("gunzip"), QStringList() << QLatin1String("-c") << file);
+                gunzip.waitForFinished();
+
+                // FIXME: Doesn't work
+                data.append(gunzip.readAll());
+            } else {
+                QFile historyFile(file);
+
+                if (historyFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+                    data.append(historyFile.readAll());
+                }
+            }
+        }
+    }
+
+    QStringList stanzas = data.split(QLatin1String("\n\n"));
+    foreach(const QString &stanza, stanzas) {
+        historyItemList << new HistoryItem(stanza);
+    }
+}
+
+History::History(QObject *parent, const QString &historyDirectory)
+       : QObject(parent)
+       , d_ptr(new HistoryPrivate(historyDirectory))
+{
+}
+
+History::~History()
+{
+    delete d_ptr;
+}
+
+HistoryItemList History::historyItems() const
+{
+    Q_D(const History);
+
+    return d->historyItemList;
+}
+
+void History::reload()
+{
+    Q_D(History);
+
+    d->init();
+}
+ 
+}
---
+++ src/history.h
@@ -0,0 +1,135 @@
+/***************************************************************************
+ *   Copyright © 2010 Jonathan Thomas <echidnaman@kubuntu.org>             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version        *
+ *   accepted by the membership of KDE e.V. (or its successor approved     *
+ *   by the membership of KDE e.V.), which shall act as a proxy            *
+ *   defined in Section 14 of version 3 of the license.                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#ifndef QAPT_HISTORY_H
+#define QAPT_HISTORY_H
+
+#include <QtCore/QDateTime>
+#include <QtCore/QList>
+#include <QtCore/QObject>
+#include <QtCore/QString>
+
+#include "package.h"
+
+/**
+ * The QApt namespace is the main namespace for LibQApt. All classes in this
+ * library fall under this namespace.
+ */
+namespace QApt {
+
+/**
+ * HistoryItemPrivate is a class containing all private members of the History class
+ */
+class HistoryItemPrivate;
+
+/**
+ * The HistoryItem class represents a single entry in the APT history logs
+ *
+ * @author Jonathan Thomas
+ * @since 1.1
+ */
+class Q_DECL_EXPORT HistoryItem
+{
+public:
+   /**
+    * Default constructor
+    *
+    * @param data The raw text from the history log
+    */
+    HistoryItem(const QString &data);
+
+   /**
+    * Default destructor
+    */
+    ~HistoryItem();
+
+   /**
+    * Returns the date upon which the transaction occurred.
+    *
+    * @return the date when the transaction occurred as a \c QDateTime
+    */
+    QDateTime startDate() const;
+
+   /**
+    * Returns the action performed during the transaction
+    *
+    * @return the action performed during the transaction as a \c Package::State
+    */
+    Package::State action() const;
+
+   /**
+    * Returns the list of packages affected by the transaction
+    *
+    * @return the package list as a \c QStringList
+    */
+    QStringList packageList() const;
+
+private:
+    Q_DECLARE_PRIVATE(HistoryItem);
+    HistoryItemPrivate *const d_ptr;
+};
+
+typedef QList<HistoryItem *> HistoryItemList;
+
+/**
+ * HistoryPrivate is a class containing all private members of the History class
+ */
+class HistoryPrivate;
+
+
+/**
+ * The History class is an interface for retreiving information from the APT
+ * history logs.
+ *
+ * @author Jonathan Thomas
+ * @since 1.1
+ */
+class Q_DECL_EXPORT History : public QObject
+{
+    Q_OBJECT
+public:
+     /**
+      * Default constructor
+      */
+    explicit History(QObject *parent, const QString &historyDirectory);
+
+     /**
+      * Default destructor
+      */
+    virtual ~History();
+
+    HistoryItemList historyItems() const;
+
+private:
+    Q_DECLARE_PRIVATE(History);
+    HistoryPrivate *const d_ptr;
+
+public Q_SLOTS:
+   /**
+    * Re-initializes the history log data. This should be connected to a
+    * directory watch (such as KDirWatch) to catch changes to the history
+    * file on-the-fly
+    */
+    void reload();
+};
+
+}
+
+#endif
