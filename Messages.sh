b0a18bc5e322310b6d2c359f043195705572d924
Jonathan Thomas <echidnaman@kubuntu.org>
2011-02-02 23:25:39 -0500

    Extract strings for the installer runner

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 $XGETTEXT `find utils/qapt-batch -name '*.cpp'` -o $podir/qaptbatch.pot
 $XGETTEXT `find utils/qapt-gst-helper -name '*.cpp'` -o $podir/qapt-gst-helper.pot
+$XGETTEXT `find utils/plasma-runner-installer -name '*.cpp'` -o $podir/plasma-runner-installer.pot
