b1c0344a160a6fab42a8c963c48a6d34dd5ccb80
Jonathan Thomas <echidnaman@kubuntu.org>
2011-02-05 22:10:18 -0500

    Extract messages from qapt-deb-installer

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 $XGETTEXT `find utils/qapt-batch -name '*.cpp'` -o $podir/qaptbatch.pot
 $XGETTEXT `find utils/qapt-gst-helper -name '*.cpp'` -o $podir/qapt-gst-helper.pot
+$XGETTEXT `find utils/qapt-deb-installer -name '*.cpp'` -o $podir/qapt-deb-installer.pot
 $XGETTEXT `find utils/plasma-runner-installer -name '*.cpp'` -o $podir/plasma-runner-installer.pot
