0362db8e0ded7d9afe6f075427cd044364b2b62e
Jonathan Thomas <echidnaman@kubuntu.org>
2011-12-19 17:42:03 -0500

    Update FindPolkitQt-1.cmake from polkit-kde-1 to be able to properly find PolkitQt-1 > 0.100

--- cmake/modules/FindPolkitQt-1.cmake
+++ cmake/modules/FindPolkitQt-1.cmake
@@ -8,92 +8,30 @@
 #  POLKITQT-1_GUI_LIBRARY - Link this to use GUI elements in polkit-qt (polkit-qt-gui)
 #  POLKITQT-1_AGENT_LIBRARY - Link this to use the agent wrapper in polkit-qt
 #  POLKITQT-1_DEFINITIONS - Compiler switches required for using Polkit-qt
+#
+# The minimum required version of PolkitQt-1 can be specified using the
+# standard syntax, e.g. find_package(PolkitQt-1 1.0)
 
-# Copyright (c) 2009, Dario Freddi, <drf@kde.org>
+# Copyright (c) 2010, Dario Freddi, <drf@kde.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if (POLKITQT-1_INCLUDE_DIR AND POLKITQT-1_LIB)
-    set(POLKITQT-1_FIND_QUIETLY TRUE)
-endif (POLKITQT-1_INCLUDE_DIR AND POLKITQT-1_LIB)
+# Support POLKITQT-1_MIN_VERSION for compatibility:
+if ( NOT PolkitQt-1_FIND_VERSION AND POLKITQT-1_MIN_VERSION )
+  set ( PolkitQt-1_FIND_VERSION ${POLKITQT-1_MIN_VERSION} )
+endif ( NOT PolkitQt-1_FIND_VERSION AND POLKITQT-1_MIN_VERSION )
 
-if (NOT POLKITQT-1_MIN_VERSION)
-  set(POLKITQT-1_MIN_VERSION "0.95.1")
-endif (NOT POLKITQT-1_MIN_VERSION)
+set( _PolkitQt-1_FIND_QUIETLY  ${PolkitQt-1_FIND_QUIETLY} )
+find_package( PolkitQt-1 ${PolkitQt-1_FIND_VERSION} QUIET NO_MODULE PATHS ${LIB_INSTALL_DIR}/PolkitQt-1/cmake )
+set( PolkitQt-1_FIND_QUIETLY ${_PolkitQt-1_FIND_QUIETLY} )
 
-if (NOT WIN32)
-   # use pkg-config to get the directories and then use these values
-   # in the FIND_PATH() and FIND_LIBRARY() calls
-   find_package(PkgConfig)
-   pkg_check_modules(PC_POLKITQT-1 polkit-qt-1)
-   set(POLKITQT-1_DEFINITIONS ${PC_POLKITQT-1_CFLAGS_OTHER})
-endif (NOT WIN32)
-
-find_path( POLKITQT-1_INCLUDE_DIR
-     NAMES polkitqt1-authority.h
-     PATH_SUFFIXES polkit-qt-1
-)
-
-find_path( POLKITQT-1_VERSION_FILE
-     NAMES polkitqt1-version.h
-     PATH_SUFFIXES polkit-qt-1
-)
-
-set(POLKITQT-1_VERSION_OK TRUE)
-if(POLKITQT-1_VERSION_FILE)
-  file(READ ${POLKITQT-1_INCLUDE_DIR}/polkitqt1-version.h POLKITQT-1_VERSION_CONTENT)
-  string (REGEX MATCH "POLKITQT1_VERSION_STRING \".*\"\n" POLKITQT-1_VERSION_MATCH "${POLKITQT-1_VERSION_CONTENT}")
-  
-  if(POLKITQT-1_VERSION_MATCH)
-    string(REGEX REPLACE "POLKITQT1_VERSION_STRING \"(.*)\"\n" "\\1" POLKITQT-1_VERSION ${POLKITQT-1_VERSION_MATCH})
-    if(POLKITQT-1_VERSION STRLESS "${POLKITQT-1_MIN_VERSION}")
-      set(POLKITQT-1_VERSION_OK FALSE)
-      if(PolkitQt-1_FIND_REQUIRED)
-        message(FATAL_ERROR "PolkitQt-1 version ${POLKITQT-1_VERSION} was found, but it is too old. Please install ${POLKITQT-1_MIN_VERSION} or newer.")
-      else(PolkitQt-1_FIND_REQUIRED)
-        message(STATUS "PolkitQt-1 version ${POLKITQT-1_VERSION} is too old. Please install ${POLKITQT-1_MIN_VERSION} or newer.")
-      endif(PolkitQt-1_FIND_REQUIRED)
-    endif(POLKITQT-1_VERSION STRLESS "${POLKITQT-1_MIN_VERSION}")
-  endif(POLKITQT-1_VERSION_MATCH)
-elseif(POLKITQT-1_INCLUDE_DIR)
-  # The version is so old that it does not even have the file
-  set(POLKITQT-1_VERSION_OK FALSE)
-  if(PolkitQt_FIND_REQUIRED)
-    message(FATAL_ERROR "It looks like PolkitQt-1 is too old. Please install PolkitQt-1 version ${POLKITQT-1_MIN_VERSION} or newer.")
-  else(PolkitQt_FIND_REQUIRED)
-    message(STATUS "It looks like PolkitQt-1 is too old. Please install PolkitQt-1 version ${POLKITQT-1_MIN_VERSION} or newer.")
-  endif(PolkitQt_FIND_REQUIRED)
-endif(POLKITQT-1_VERSION_FILE)
-
-find_library( POLKITQT-1_CORE_LIBRARY 
-    NAMES polkit-qt-core-1
-    HINTS ${PC_POLKITQT-1_LIBDIR}
-)
-find_library( POLKITQT-1_GUI_LIBRARY 
-    NAMES polkit-qt-gui-1
-    HINTS ${PC_POLKITQT-1_LIBDIR}
-)
-find_library( POLKITQT-1_AGENT_LIBRARY 
-    NAMES polkit-qt-agent-1
-    HINTS ${PC_POLKITQT-1_LIBDIR}
-)
-
-set(POLKITQT-1_LIBRARIES ${POLKITQT-1_GUI_LIBRARY} ${POLKITQT-1_CORE_LIBRARY} ${POLKITQT-1_AGENT_LIBRARY})
-
-include(FindPackageHandleStandardArgs)
-
-# handle the QUIETLY and REQUIRED arguments and set POLKITQT-1_FOUND to TRUE if 
-# all listed variables are TRUE
-find_package_handle_standard_args(PolkitQt-1 DEFAULT_MSG POLKITQT-1_LIBRARIES POLKITQT-1_INCLUDE_DIR POLKITQT-1_VERSION_OK)
-
-mark_as_advanced(POLKITQT-1_INCLUDE_DIR POLKITQT-1_CORE_LIBRARY POLKITQT-1_GUI_LIBRARY POLKITQT-1_LIBRARIES POLKITQT-1_VERSION_OK)
+include( FindPackageHandleStandardArgs )
+find_package_handle_standard_args( PolkitQt-1 DEFAULT_MSG PolkitQt-1_CONFIG )
 
 if (POLKITQT-1_FOUND)
-    if (NOT PC_POLKITQT-1_PREFIX STREQUAL CMAKE_INSTALL_PREFIX)
+    if (NOT POLKITQT-1_INSTALL_DIR STREQUAL CMAKE_INSTALL_PREFIX)
         message("WARNING: Installation prefix does not match PolicyKit install prefixes. You probably will need to move files installed "
-                "in POLICY_FILES_INSTALL_DIR and by dbus_add_activation_system_service to the ${PC_POLKITQT-1_PREFIX} prefix")
-    endif (NOT PC_POLKITQT-1_PREFIX STREQUAL CMAKE_INSTALL_PREFIX)
+                "in POLICY_FILES_INSTALL_DIR and by dbus_add_activation_system_service to the ${POLKITQT-1_INSTALL_DIR} prefix")
+    endif (NOT POLKITQT-1_INSTALL_DIR STREQUAL CMAKE_INSTALL_PREFIX)
 endif (POLKITQT-1_FOUND)
-
-set(POLKITQT-1_POLICY_FILES_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/share/polkit-1/actions)
