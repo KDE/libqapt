KDE/libqapt

commit fc6f54ecefe5a791ce130896fe1dacde1c8e5a83

Fix ambiguous overload errors encountered with newer versions of APT.

src/backend.cpp
@@ -279,7 +279,7 @@ void Backend::reloadCache()
         if (!pinFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
             continue;
         }
-        FileFd Fd(pinFile.handle(), FileFd::ReadOnly);
+        FileFd Fd(pinFile.handle(), FileFd::ReadOnly, FileFd::Auto);
 
         pkgTagFile tagFile(&Fd);
         if (_error->PendingError()) {
@@ -1329,7 +1329,7 @@ bool Backend::setPackagePinned(Package *package, bool pin)
             if (!pinFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
                 continue;
             }
-            FileFd Fd(pinFile.handle(), FileFd::ReadOnly);
+            FileFd Fd(pinFile.handle(), FileFd::ReadOnly, FileFd::Auto);
 
             pkgTagFile tagFile(&Fd);
             if (_error->PendingError()) {

commit 2e66378852f57880a3da08fa543f2d3771e27762

Pin files are uncompressed, so don't open then with auto compression detection.
Fixes an init error caused by fc6f54ec.

src/backend.cpp
@@ -279,7 +279,7 @@ void Backend::reloadCache()
         if (!pinFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
             continue;
         }
-        FileFd Fd(pinFile.handle(), FileFd::ReadOnly, FileFd::Auto);
+        FileFd Fd(pinFile.handle(), FileFd::ReadOnly, FileFd::None);
 
         pkgTagFile tagFile(&Fd);
         if (_error->PendingError()) {
@@ -1329,7 +1329,7 @@ bool Backend::setPackagePinned(Package *package, bool pin)
             if (!pinFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
                 continue;
             }
-            FileFd Fd(pinFile.handle(), FileFd::ReadOnly, FileFd::Auto);
+            FileFd Fd(pinFile.handle(), FileFd::ReadOnly, FileFd::None);
 
             pkgTagFile tagFile(&Fd);
             if (_error->PendingError()) {