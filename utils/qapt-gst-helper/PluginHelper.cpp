11f09a545242152e760fbbaa60f90bea3f557b6b
Jonathan Thomas <echidnaman@kubuntu.org>
2012-02-26 13:50:57 -0500

    Give the user notification that we couldn't find any plugins.

--- utils/qapt-gst-helper/PluginHelper.cpp
+++ utils/qapt-gst-helper/PluginHelper.cpp
@@ -484,6 +484,9 @@ void PluginHelper::incrementProgress()
     progressBar()->setValue(progressBar()->value() + 1);
     if (progressBar()->value() == progressBar()->maximum()) {
         if (m_foundCodecs.isEmpty()) {
+            QString text = i18nc("@info", "No plugins could be found");
+            QString title = i18nc("@title", "Plugins Not Found");
+            KMessageBox::error(this, text, title);
             exit(ERR_NO_PLUGINS);
         }
         offerInstallPackages();
