9ebdc1d7d120c9a8024e1d66c355c955b7dc90a1
Jonathan Thomas <echidnaman@kubuntu.org>
2012-10-13 12:02:51 -0400

    Set the debconf frontend to noninteractive if a client forgets to set the debconf pipe.

--- src/worker/workerinstallprogress.cpp
+++ src/worker/workerinstallprogress.cpp
@@ -48,7 +48,6 @@ WorkerInstallProgress::WorkerInstallProgress(QObject* parent, int begin, int end
         , m_progressBegin(begin)
         , m_progressEnd(end)
 {
-    setenv("DEBIAN_FRONTEND", "passthrough", 1);
     setenv("APT_LISTBUGS_FRONTEND", "none", 1);
     setenv("APT_LISTCHANGES_FRONTEND", "debconf", 1);
 
@@ -59,7 +58,13 @@ void WorkerInstallProgress::setTransaction(Transaction *trans)
 {
     m_trans = trans;
     std::setlocale(LC_ALL, m_trans->locale().toAscii());
-    setenv("DEBCONF_PIPE", trans->debconfPipe().toAscii(), 1);
+
+    if (!trans->debconfPipe().isEmpty()) {
+        setenv("DEBIAN_FRONTEND", "passthrough", 1);
+        setenv("DEBCONF_PIPE", trans->debconfPipe().toAscii(), 1);
+    } else {
+        setenv("DEBIAN_FRONTEND", "noninteractive", 1);
+    }
 }
 
 pkgPackageManager::OrderResult WorkerInstallProgress::start(pkgPackageManager *pm)
