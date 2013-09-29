11dc5be8a03a08fc0395f2b64c49df4438568c35
Burkhard Lück <lueck@hube-lueck.de>
2013-09-29 14:58:54 +0200

    fix wrong catalog name (cherry picked from commit c9768498c7fd901609974e0373bc3b88182bf7d6)

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,4 @@
 $XGETTEXT `find utils/qapt-batch -name '*.cpp'` -o $podir/qaptbatch.pot
 $XGETTEXT `find utils/qapt-gst-helper -name '*.cpp'` -o $podir/qapt-gst-helper.pot
 $XGETTEXT `find utils/qapt-deb-installer -name '*.cpp'` -o $podir/qapt-deb-installer.pot
-$XGETTEXT `find utils/plasma-runner-installer -name '*.cpp'` -o $podir/plasma-runner-installer.pot
+$XGETTEXT `find utils/plasma-runner-installer -name '*.cpp'` -o $podir/plasma_runner_installer.pot
