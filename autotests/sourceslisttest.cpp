c5951f8fc502784901f471b8aa0f624090612e45
Harald Sitter <sitter@kde.org>
2014-07-14 16:16:46 +0200

    bring back tests as autotests (in line with frameworks naming that is)
    otherwise straight port to ecm macros

--- autotests/sourceslisttest.cpp
+++ autotests/sourceslisttest.cpp
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-#include "../tests/sourceslisttest.h"
+#include "sourceslisttest.h"
 
 #include <QtTest/QtTest>
 
