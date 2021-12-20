5243538c6c2b2aa434b965840226432aa96f14c0
Alexander Volkov <avolkov@astralinux.ru>
2021-12-20 16:47:15 +0300

    Load translations for the library

--- Messages.sh
+++ Messages.sh
@@ -3,3 +3,4 @@ $XGETTEXT `find utils/qapt-batch -name '*.cpp'` -o $podir/qaptbatch.pot
 $XGETTEXT `find utils/qapt-gst-helper -name '*.cpp'` -o $podir/qapt-gst-helper.pot
 $XGETTEXT `find utils/qapt-deb-installer -name '*.cpp'` -o $podir/qapt-deb-installer.pot
 $XGETTEXT `find utils/plasma-runner-installer -name '*.cpp'` -o $podir/plasma_runner_installer.pot
+$EXTRACT_TR_STRINGS `find src -name '*.cpp'` -o $podir/libqapt.pot
